ff3efb8be47b7e2ba7df5dd3f105673a41128956
MaxWuBlouin <maxwublouin@gmail.com>
2024-11-30 16:13:30 -0500

    Simplified function signatures

--- bms-firmware/src/bms_interface.cpp
+++ bms-firmware/src/bms_interface.cpp
@@ -4,11 +4,9 @@
 
 
 namespace bms_interface {
-    int set_cell_overvoltage(byte i2c_addr, int voltage_mv, int delay_s) {
-        return 0;
+    void set_cell_overvoltage(byte i2c_addr, int voltage_mv)  {
     }
 
-    int set_cell_undervoltage(byte i2c_addr, int voltage_mv, int delay_s) {
-        return 0;
+    void set_cell_undervoltage(byte i2c_addr, int voltage_mv) {
     }
 }
--- bms-firmware/src/bms_interface.h
+++ bms-firmware/src/bms_interface.h
@@ -4,10 +4,8 @@
 #define BMS_INTERFACE
 
 namespace bms_interface {
-    int set_cell_overvoltage_protection(byte i2c_addr, int voltage_mv, int delay_s);
-    int set_cell_undervoltage_protection(byte i2c_addr, int voltage_mv, int delay_s);
-
-    long set_short_circuit_protection(byte i2c_addr, long current_ma, int delay_s);
+    void set_cell_overvoltage_protection(byte i2c_addr, int voltage_mv);
+    void set_cell_undervoltage_protection(byte i2c_addr, int voltage_mv);
 }
 
 #endif
