f7c27b566aeeb39a38d337a1972c9102c2ee18c2
MaxWuBlouin <maxwublouin@gmail.com>
2024-11-30 13:37:21 -0500

    Established logic for setup() and loop()

--- bms-firmware/src/main.cpp
+++ bms-firmware/src/main.cpp
@@ -16,6 +16,7 @@
 #define UV_THRESH_MV 3500
 
 #define BMS_I2C_ADDR_1 0x08
+#define BMS_I2C_ADDR_2 0x0C // TODO: find proper address for 2nd BMS chip
 
 #define CELLS_PER_BATTERY 10
 
@@ -45,17 +46,37 @@ void publish_bms_data(BMS_controller*);
 void setup() {
   Serial.begin(9600);
   last_time_ms = millis();
+
+  bms_controller_1.name = "BMS1";
+  bms_controller_1.address = BMS_I2C_ADDR_1;
+  bms_controller_2.name = "BMS2";
+  bms_controller_2.address = BMS_I2C_ADDR_2;
+
+  upload_bms_setpoints(&bms_controller_1);
+  // upload_bms_setpoints(&bms_controller_2);
 }
 
 void loop() {
   if(millis() - last_time_ms > PUBLISH_RATE_MS){
+    read_bms_data(&bms_controller_1);
     publish_bms_data(&bms_controller_1);
+    // read_bms_data(&bms_controller_2);
+    // publish_bms_data(&bms_controller_2);
+    
     last_time_ms = millis();  
   }
 }
 
 
 // Function definitions
+void upload_bms_setpoints(BMS_controller *bms_controller) {
+
+}
+
+void read_bms_data(BMS_controller *bms_controller) {
+
+}
+
 void publish_bms_data(BMS_controller *bms_controller){
     Serial.print(bms_controller->name);
     for (int i = 0; i<CELLS_PER_BATTERY; i++){
