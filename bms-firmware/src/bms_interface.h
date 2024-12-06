8c5a67ceb712694fa0227b5eae721f677f1473fd
MaxWuBlouin <maxwublouin@gmail.com>
2024-12-06 13:44:00 -0500

    header file for bms interface reflects documentation

--- bms-firmware/src/bms_interface.h
+++ bms-firmware/src/bms_interface.h
@@ -4,8 +4,12 @@
 #define BMS_INTERFACE
 
 namespace bms_interface {
+    void enable_adc(byte i2c_addr);
+    void enable_discharge_mosfet(byte i2c_addr);
     void set_cell_overvoltage_protection(byte i2c_addr, int voltage_mv);
     void set_cell_undervoltage_protection(byte i2c_addr, int voltage_mv);
+    void set_overcurrent_discharge_protection(byte i2c_addr, int current_ma);
+    void set_short_circuit_protection(byte i2c_addr, int current_ma);
 }
 
 #endif
