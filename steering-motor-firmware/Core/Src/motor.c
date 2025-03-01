d18acc8010f8294165c05d0c74d9e3b934c95b8e
Anthony Ozerov <Westernham123@gmail.com>
2025-03-01 13:27:55 -0500

    Fixed Direction :)

--- steering-motor-firmware/Core/Src/motor.c
+++ steering-motor-firmware/Core/Src/motor.c
@@ -4,5 +4,11 @@ void set_motor_speed(int n){
 	TIM8->CCR2 = n;
 }
 void set_motor_direction(int n){
-	HAL_GPIO_WritePin(DIR_GPIO_Port, DIR_Pin, GPIO_PIN_RESET);
+
+	if (n == 1) {
+		HAL_GPIO_WritePin(DIR_GPIO_Port, DIR_Pin, GPIO_PIN_RESET);
+	}
+	else {
+		HAL_GPIO_WritePin(DIR_GPIO_Port, DIR_Pin, GPIO_PIN_SET);
+	}
 }
