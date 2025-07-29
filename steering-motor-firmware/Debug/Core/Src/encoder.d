59ce680f9fa153c5333d103e86b08d204aa12b78
Vincent <vincentvibe3@gmail.com>
2025-07-28 23:06:51 -0400

    adjust encoder mode direction

--- steering-motor-firmware/Debug/Core/Src/encoder.d
+++ steering-motor-firmware/Debug/Core/Src/encoder.d
@@ -29,7 +29,7 @@ Core/Src/encoder.o: ../Core/Src/encoder.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
  ../Core/Inc/encoder.h ../Core/Inc/CAN_processing.h ../Core/Inc/main.h \
- ../Core/Inc/pid.h
+ ../Core/Inc/pid.h ../Core/Inc/calibration.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -64,3 +64,4 @@ Core/Src/encoder.o: ../Core/Src/encoder.c ../Core/Inc/main.h \
 ../Core/Inc/CAN_processing.h:
 ../Core/Inc/main.h:
 ../Core/Inc/pid.h:
+../Core/Inc/calibration.h:
