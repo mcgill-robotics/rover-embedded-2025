3123807965c22f9c773459c68e03a01981aa938a
Dragos Bajanica <bajanica.dragos@gmail.com>
2026-01-31 17:15:24 -0500

    Fixed UART-to-USB communication cutting off at 64 bytes

--- uart-board/Core/Src/deserialization.cc
+++ uart-board/Core/Src/deserialization.cc
@@ -1,4 +1,3 @@
-
 #include "ArduinoJson.h"
 #include "ArduinoJson/Document/JsonDocument.hpp"
 #include "ArduinoJson/Json/JsonDeserializer.hpp"
--- uart-board/Core/Src/serialization.cc
+++ uart-board/Core/Src/serialization.cc
@@ -4,20 +4,30 @@
 #include "ArduinoJson/Document/JsonDocument.hpp"
 #include "ArduinoJson/Json/JsonSerializer.hpp"
 #include "class/cdc/cdc_device.h"
+#include <stdint.h>
 
 extern "C" {
-void serialize(uint8_t *output, size_t output_len, const char *topic,
-               uint8_t *msg, size_t msg_len) {
+size_t serialize(uint8_t *output, size_t output_len, const char *topic,
+               uint8_t *msg) {
   JsonDocument doc;
   doc["topic"] = topic;
   doc["message"] = msg;
-  serializeJson(doc, (void *)output, output_len);
+  return serializeJson(doc, (void *)output, output_len);
 }
 
-void send_msg(const char *topic, uint8_t *msg, size_t msg_len) {
-  uint8_t json[JSON_BUF_LEN] = {0};
-  serialize(json, JSON_BUF_LEN, topic, msg, msg_len);
+void send_msg(const char *topic, uint8_t *msg) {
+  uint8_t json[JSON_BUF_LEN];
+  uint8_t* json_ptr = json;
+  size_t json_size = serialize(json, JSON_BUF_LEN, topic, msg);
 
-  tud_cdc_n_write(0, json, JSON_BUF_LEN);
+  uint32_t to_write = json_size;
+  while (to_write > 0) {
+      uint32_t written = tud_cdc_n_write(0, json_ptr, to_write);
+      to_write -= written;
+      json_ptr += written;
+  }
+  
+  tud_cdc_n_write(0, "\n", 1);
+  tud_cdc_n_write_flush(0);
 }
 }
