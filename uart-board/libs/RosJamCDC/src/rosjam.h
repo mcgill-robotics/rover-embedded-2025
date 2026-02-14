7465b18e647739f0c20acb3ff70666209bf43e3f
Vincent <Vincentvibe3@gmail.com>
2026-02-14 16:44:24 -0500

    Define rosjam lib interface

--- uart-board/libs/RosJamCDC/src/rosjam.h
+++ uart-board/libs/RosJamCDC/src/rosjam.h
@@ -1,7 +1,20 @@
 #ifndef ROSJAM_H
 #define ROSJAM_H
 
-void enable_interface(int itf, UART_HandleTypeDef huart);
-void set_transform(int itf);
+#include <cstdint>
+typedef struct rosjam_endpoint {
+	char topic[20];
+	int id;
+	uint8_t rx_buf[2048];
+	uint8_t tx_buf[2048];
+} rosjam_endpoint;
+
+void enable_interface(rosjam_endpoint* endpoint, UART_HandleTypeDef huart);
+
+void sendToUSB(rosjam_endpoint* endpoint, char* message);
+
+void receivedFromUSB(char* id, char* message);
+
+void process();
 
 #endif
