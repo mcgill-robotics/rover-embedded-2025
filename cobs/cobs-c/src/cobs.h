0564736a5e429e54214d51a9ba9d3e7f3b0046bb
Vincent <Vincentvibe3@gmail.com>
2026-04-22 10:59:55 -0400

    Rearrange cobs code

---
+++ cobs/cobs-c/src/cobs.h
@@ -0,0 +1,6 @@
+#ifndef COBS_H
+#define COBS_H
+#include <stdint.h>
+int decode(uint8_t* input, int input_length, uint8_t* output, int output_length, uint8_t delim, int* written);
+int encode(uint8_t* input, int input_length, uint8_t* output, int output_length, uint8_t delim);
+#endif
