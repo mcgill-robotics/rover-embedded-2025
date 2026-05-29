0ef611b31fa4d2402cf6993f5d67ad4d034dc3d3
Vincent <Vincentvibe3@gmail.com>
2026-05-29 19:34:31 -0400

    Adjust tmux args

--- CAN_Interface/arm_tmux.sh
+++ CAN_Interface/arm_tmux.sh
@@ -1,5 +1,5 @@
 TERM=xterm
-tmux new-session -L -d -s logger "./start_dashboard.sh"
+tmux new-session -d -s logger "./start_dashboard.sh"
 tmux split-window -h
 tmux send-keys -t 1 "./start_logger.sh" C-m
 tmux attach -t logger
