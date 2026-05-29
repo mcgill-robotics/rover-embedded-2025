530607796757f391fa6d855e48d80653ca1fe704
Vincent <Vincentvibe3@gmail.com>
2026-05-29 19:32:09 -0400

    Add more arm startup scripts

--- CAN_Interface/arm_tmux.sh
+++ CAN_Interface/arm_tmux.sh
@@ -1,5 +1,5 @@
 TERM=xterm
-tmux new-session -d -s logger "python3 scripts/can_logger.py --port /dev/ttyACM0 --db can_log.db"
+tmux new-session -L -d -s logger "./start_dashboard.sh"
 tmux split-window -h
-tmux send-keys -t 1 "python3 scripts/can_dashboard.py --db can_log.db" C-m
+tmux send-keys -t 1 "./start_logger.sh" C-m
 tmux attach -t logger
